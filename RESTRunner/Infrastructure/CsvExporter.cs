4a097b2b19a0681bb2550003d3b7b1da6675990d
Mark Hazleton <mark.hazleton@controlorigins.com>
2022-06-13 00:03:27 -0500

    updates

--- RESTRunner/Infrastructure/CsvExporter.cs
+++ RESTRunner/Infrastructure/CsvExporter.cs
@@ -1,23 +1,4 @@
 ﻿namespace RESTRunner.Infrastructure;
-
-/// <summary>
-/// Export File Name
-/// </summary>
-public class ExportFileVM
-{
-    /// <summary>
-    /// Export File Name
-    /// </summary>
-    public string ExportFileName = string.Empty;
-    /// <summary>
-    /// Content Type
-    /// </summary>
-    public string ContentType = string.Empty;
-    /// <summary>
-    /// Data to export
-    /// </summary>
-    public byte[]? Data;
-}
 /// <summary>
 /// Extension class to Export CompareResult list to a CSV file
 /// </summary>
