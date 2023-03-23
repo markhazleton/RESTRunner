c3ac7641f75df7f7cfc1a593534e03e03bfbec50
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-03-23 09:44:30 -0500

    Update to use IHttpClientFactory to get HttpClient

--- RESTRunner/Program.cs
+++ RESTRunner/Program.cs
@@ -1,8 +1,36 @@
-﻿var myRunner = new CompareRunner();
-myRunner.InitializeCompareRunner();
-var import = new PostmanImport(myRunner);
-import.LoadFromPostman("collection.json");
-myRunner.SaveToFile();
-var ExecuteRunner = new ExecuteRunnerService(myRunner);
-await ExecuteRunner.ExecuteRunnerAsync(new CsvOutput()).ConfigureAwait(false);
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
+var builder = new HostBuilder()
+.ConfigureServices((hostContext, services) =>
+{
+    services.AddHttpClient();
+    services.AddSingleton(serviceProvider =>
+    {
+        var myRunner = new CompareRunner();
+        myRunner.InitializeCompareRunner();
+        var import = new PostmanImport(myRunner);
+        import.LoadFromPostman("collection.json");
+        myRunner.SaveToFile();
+        return myRunner;
+    });
+    services.AddTransient<ExecuteRunnerService>();
+}).UseConsoleLifetime();
+
+
+var host = builder.Build();
+
+using (var serviceScope = host.Services.CreateScope())
+{
+    var services = serviceScope.ServiceProvider;
+    try
+    {
+        var myService = services.GetRequiredService<ExecuteRunnerService>();
+        await myService.ExecuteRunnerAsync(new CsvOutput()).ConfigureAwait(false);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error Occured:{ex.Message}");
+    }
+}
+return 0;
