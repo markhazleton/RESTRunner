e517480ce88d6d8f867c36d72f5124e34b286cf3
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-10-07 09:11:08 -0500

    Implement generated REST Client

---
+++ RESTRunner.Web/SampleCRUD/SampleCRUDService.cs
@@ -0,0 +1,21 @@
+﻿namespace RESTRunner.Web.SampleCRUD
+{
+    public class SampleCRUDService
+    {
+        public SampleCRUDService()
+        {
+                
+        }
+        public async Task<string> GetEmployeeCount()
+        {
+            HttpClient client = new()
+            {
+                BaseAddress = new Uri("https://markhazletonsamplecrud.controlorigins.com/")
+            };
+            SampleCRUDClient sampleCRUClients = new(client);
+            var employees = await sampleCRUClients.EmployeeAllAsync(1, 5, "1");
+            return $"Got {employees.Count} Employees";
+        }
+
+    }
+}
